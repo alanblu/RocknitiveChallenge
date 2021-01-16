272f28a9873bc0b906353489b208b925b31b20c0
alanblu <alanblu@ieee.org>
2021-01-16 23:38:54 +0100

    Update challenge.c
    I use a buffer instead of trying to catch the exact moment for the data asquisition

--- src/challenge.c
+++ src/challenge.c
@@ -8,19 +8,19 @@
 #define CHALLENGE_TYPE_DELAY   (0x43)
 #define CHALLENGE_TYPE_TIMEOUT (0x51)
 #define CHALLENGE_TYPE_LOG     (0x60)
+#define CHALLENGE_BUFFER_SIZE  (16  )
 
-static uint8_t Challenge_typeID;
-static uint8_t Challenge_dataLength;
-static uint8_t Challenge_pendingData;
+static uint8_t Challenge_buffer_data[CHALLENGE_BUFFER_SIZE];
+
 static uint8_t Challenge_dataCounter;
-static uint8_t Challenge_dataBuffer[10];
 static TickType_t Challenge_start;
 static TickType_t Challenge_timeOut;
 
-static void Challenge_HandlerEmpty();
-static void Challenge_HandlerAdd();
+static void Challenge_HandlerEmpty(uint8_t data[], uint16_t lenght, uint16_t idx );
+static void Challenge_HandlerAdd(uint8_t data[], uint16_t lenght, uint16_t init) ;
+static void Challenge_DumpData(uint8_t data[], uint16_t lenght, uint16_t init);
 static void Challenge_HandlerResult();
-static void Challenge_HandlerDelay();
+static void Challenge_HandlerDelay(uint8_t data[], uint16_t lenght, uint16_t idx);
 static void Challenge_HandlerTimeOut();
 static void Challenge_HandlerLog();
 
@@ -37,20 +37,10 @@ void send(uint8_t* message, uint32_t length);
  */
 void receive_ISR(uint8_t data) {
 
-    if(Challenge_pendingData == 1)
-    {  
-       if(Challenge_dataCounter < Challenge_dataLength)
-       {
-           Challenge_dataBuffer[    Challenge_dataCounter] = data;
-           Challenge_dataCounter++;
-       }       
-       
-    }
-    else
-    {
-        Challenge_typeID = data;
-        Challenge_dataLength = data & 0x0F;
-    } 
+    if(Challenge_dataCounter>CHALLENGE_BUFFER_SIZE) Challenge_dataCounter = 0;
+
+    Challenge_buffer_data[Challenge_dataCounter] = data;
+    Challenge_dataCounter++;
 
 }
 
@@ -60,9 +50,6 @@ void receive_ISR(uint8_t data) {
 void challenge_init() {
     Challenge_start = xTaskGetTickCount();
     Challenge_timeOut = 0xFFFFFFFF;
-    Challenge_typeID = 0xFF; // NOT INIT
-    Challenge_dataLength = 0xFF; // NOT INIT
-    Challenge_pendingData = 0;
     Challenge_dataCounter = 0;
 }
 
@@ -84,25 +71,29 @@ void challenge_init() {
  */
 void challenge_run() 
 {
+    uint32_t idx = 0;
+
     while(1)
     {
+        if(idx >= CHALLENGE_BUFFER_SIZE) idx = 0;
+
         TickType_t elapsed = xTaskGetTickCount() - Challenge_start;
-        //if(elapsed > Challenge_timeOut) Challenge_typeID = CHALLENGE_TYPE_TIMEOUT;
+        if(elapsed > Challenge_timeOut) Challenge_HandlerTimeOut();
         //console_print("ERROR: Hola 0x%x\n",Challenge_pendingData );
 
-        vTaskDelay(10);    
+        vTaskDelay(100);    
+        
 
-        switch(Challenge_typeID)
+        switch(Challenge_buffer_data[0])
         {
             case CHALLENGE_TYPE_EMPTY:
-            { 
-                Challenge_pendingData = 1;                      
-                Challenge_HandlerEmpty();
+            {                       
+                Challenge_HandlerEmpty(&Challenge_buffer_data[0], 1, 0);
                 break;
             }
             case CHALLENGE_TYPE_ADD:
             {
-                Challenge_HandlerAdd();
+                Challenge_HandlerAdd(&Challenge_buffer_data[0], 4, 0);
                 break;
             }
             case CHALLENGE_TYPE_RESULT:
@@ -112,12 +103,11 @@ void challenge_run()
             }
             case CHALLENGE_TYPE_DELAY:
             {               
-                Challenge_HandlerDelay();
+                Challenge_HandlerDelay(&Challenge_buffer_data[0], 3, 0);
                 break;
             }
             case CHALLENGE_TYPE_TIMEOUT:
             {
-                Challenge_HandlerTimeOut();
                 break;
             }
             case CHALLENGE_TYPE_LOG:
@@ -126,47 +116,39 @@ void challenge_run()
             }
             default:
             {
+                
+                //console_print("Result en ADD en challenge 0x%x \n", Challenge_buffer_data[idx] );
+                Challenge_DumpData(&Challenge_buffer_data[0], 1, 0);
                 //Challenge_pendingData = 0;
                 //Error
             }
         }
-        
+
+
     }
     // TODO: insert awesome stuff here
 
 }
 
-void Challenge_HandlerEmpty()
+void Challenge_HandlerEmpty(uint8_t data[], uint16_t lenght, uint16_t idx )
 {
-    Challenge_pendingData = 0;
-    Challenge_dataCounter = 0;
+    Challenge_DumpData(data, lenght, idx);
     uint8_t message[] = {CHALLENGE_TYPE_EMPTY};
     send(message, sizeof(message));
 }
 
-void Challenge_HandlerAdd()
+void Challenge_HandlerAdd(uint8_t data[], uint16_t lenght, uint16_t idx )
 {
     uint8_t response[3];   
-
-    if(Challenge_dataCounter < Challenge_dataLength)
-    {
-        Challenge_pendingData = 1;
-    }
-    else
-    {
-        Challenge_pendingData = 0;
-        Challenge_dataCounter = 0;
         
-        uint16_t r = (Challenge_dataBuffer[0] + Challenge_dataBuffer[2]) * 256 + Challenge_dataBuffer[1] + Challenge_dataBuffer[3];
+    uint16_t r = (data[1] + data[3]) * 256 + data[2] + data[4];
 
-        response[0] = 0x32;
-        response[1] = r>>8;
-        response[2] = r;
+    response[0] = 0x32;
+    response[1] = r>>8;
+    response[2] = r;
 
-        //console_print("Result en ADD %x %x %x\n", response[0], response[1], response[2]);
-        send(response, sizeof(response));      
-    }    
-
+    Challenge_DumpData(data, lenght, idx);
+    send(response, sizeof(response));
 }
 
 void Challenge_HandlerResult()
@@ -174,34 +156,54 @@ void Challenge_HandlerResult()
 
 }
 
-void Challenge_HandlerDelay()
+void Challenge_HandlerDelay(uint8_t data[], uint16_t lenght, uint16_t idx)
 {
-    //console_print("ERROR: Hola 3\n");
-    uint8_t delay[Challenge_dataLength];
-    uint8_t response[4];
+    uint8_t response[3];   
+        
+    uint16_t r = data[1] * 256 + data[2];
 
-    if(Challenge_dataCounter < Challenge_dataLength)
-    {
-        Challenge_pendingData = 1;
-        delay[Challenge_dataCounter] = Challenge_dataBuffer[Challenge_dataCounter];
-    }
-    else
-    {
-        Challenge_timeOut = (Challenge_dataBuffer[1]) * 256 + Challenge_dataBuffer[0];      
-    }
+    response[0] = 0x32;
+    response[1] = r>>8;
+    response[2] = r;
+
+    Challenge_DumpData(data, lenght, idx);
+
+    Challenge_timeOut =   r;   
 }
 
 void Challenge_HandlerTimeOut()
 {
-    Challenge_pendingData = 0;
-    Challenge_dataCounter = 0;
-    uint8_t response[4];
-    response[0] = 0x51;
+    uint8_t response[1];
+    response[0] = CHALLENGE_TYPE_TIMEOUT;
     send(response, 1);
-
 }
 
 void Challenge_HandlerLog()
 {
+
+}
+
+void Challenge_DumpData(uint8_t data[], uint16_t lenght, uint16_t init)
+{
+    /*console_print("\n");
+    console_print("Result en ADD\n");
+    for(int idx2 = 0; idx2 < CHALLENGE_BUFFER_SIZE; idx2++ )
+    {
+        console_print(" %x ", data[idx2]);       
+    }
+    console_print("\n");*/
+    
+    Challenge_dataCounter=-lenght;
+    for(int idx = init; idx < CHALLENGE_BUFFER_SIZE - lenght; idx++ )
+    {
+        data[idx] = data[idx + lenght + 1];
+    }
+
+    /*console_print("Result despues ADD \n");
+    for(int idx2 = 0; idx2 < CHALLENGE_BUFFER_SIZE; idx2++ )
+    {
+        console_print(" %x ", data[idx2]);       
+    }
+    console_print("\n");*/
 
 }
